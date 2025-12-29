6f1d418aa21293d9aef35ae6fd481f0e9cab0d3e
Rakesh <rakeshsunny0987@gmail.com>
2025-12-29 15:13:29 +0530

    c

---
+++ menu.py
@@ -0,0 +1,9 @@
+def menu():
+    print('-'*50)
+    print('ATM OR Funds Transfer Operations')
+    print('-'*50)
+    print('\t1.deposit')
+    print('\t2.withdraw')
+    print('\t3.check balance')
+    print('\t4.Exit')
+    print('-'*50)
